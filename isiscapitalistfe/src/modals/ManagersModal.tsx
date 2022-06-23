13ebf094649f12ccc138f7b6190089728e37bb0f
Alwenna <alwennasalaun.pro@gmail.com>
2022-06-23 18:15:09 +0200

    updated modals

--- isiscapitalistfe/src/modals/ManagersModal.tsx
+++ isiscapitalistfe/src/modals/ManagersModal.tsx
@@ -22,70 +22,82 @@ export default function ManagersModal({ loadworld }: ProductProps) {
               <div className="managersComponent">
                 <img src={"http://localhost:4000/" + world.managers[0].logo} />
                 <div>
-                  <div>{world.managers[0].name}</div>
-                  <div>
-                    {world.products[world.managers[0].idcible - 1].name}
-                  </div>
+                  <div className="managerName">{world.managers[0].name}</div>
+                  <div className="managerText">
+                  <div>{world.products[world.managers[0].idcible - 1].name}</div>
                   <div>{world.managers[0].seuil}</div>
+                  </div>
                 </div>
-                <div>Engager</div>
+                <div className="hirebutton">
+                        <div>Engager</div>
+                </div>
               </div>
               <div className="managersComponent">
                 <img src={"http://localhost:4000/" + world.managers[1].logo} />
                 <div>
-                  <div>{world.managers[1].name}</div>
-                  <div>
-                    {world.products[world.managers[1].idcible - 1].name}
-                  </div>
+                  <div className="managerName">{world.managers[1].name}</div>
+                  <div className="managerText">
+                  <div>{world.products[world.managers[1].idcible - 1].name}</div>
                   <div>{world.managers[1].seuil}</div>
+                  </div>
                 </div>
-                <div>Engager</div>
+                <div className="hirebutton">
+                        <div>Engager</div>
+                </div>
               </div>
               <div className="managersComponent">
                 <img src={"http://localhost:4000/" + world.managers[2].logo} />
                 <div>
-                  <div>{world.managers[2].name}</div>
-                  <div>
-                    {world.products[world.managers[2].idcible - 1].name}
-                  </div>
+                  <div className="managerName">{world.managers[2].name}</div>
+                  <div className="managerText">
+                  <div>{world.products[world.managers[2].idcible - 1].name}</div>
                   <div>{world.managers[2].seuil}</div>
+                  </div>
                 </div>
-                <div>Engager</div>
+                <div className="hirebutton">
+                        <div>Engager</div>
+                </div>
               </div>
             </div>
             <div className="managersRightSide">
               <div className="managersComponent">
                 <img src={"http://localhost:4000/" + world.managers[3].logo} />
                 <div>
-                  <div>{world.managers[3].name}</div>
-                  <div>
-                    {world.products[world.managers[3].idcible - 1].name}
-                  </div>
+                  <div className="managerName">{world.managers[3].name}</div>
+                  <div className="managerText">
+                  <div>{world.products[world.managers[3].idcible - 1].name}</div>
                   <div>{world.managers[3].seuil}</div>
+                  </div>
                 </div>
-                <div>Engager</div>
+                <div className="hirebutton">
+                        <div>Engager</div>
+                </div>
               </div>
               <div className="managersComponent">
                 <img src={"http://localhost:4000/" + world.managers[4].logo} />
                 <div>
-                  <div>{world.managers[4].name}</div>
-                  <div>
-                    {world.products[world.managers[4].idcible - 1].name}
-                  </div>
+                  <div className="managerName">{world.managers[4].name}</div>
+                  <div className="managerText">
+                  <div>{world.products[world.managers[4].idcible - 1].name}</div>
                   <div>{world.managers[4].seuil}</div>
+                  </div>
                 </div>
-                <div>Engager</div>
+                <div className="hirebutton">
+                        <div>Engager</div>
+                </div>
               </div>
               <div className="managersComponent">
                 <img src={"http://localhost:4000/" + world.managers[5].logo} />
                 <div>
-                  <div>{world.managers[5].name}</div>
-                  <div>
-                    {world.products[world.managers[5].idcible - 1].name}
-                  </div>
+                  <div className="managerName">{world.managers[5].name}</div>
+                  <div className="managerText">
+                  <div>{world.products[world.managers[5].idcible - 1].name}</div>
                   <div>{world.managers[5].seuil}</div>
+                  </div>
                 </div>
-                <div>Engager</div>
+                <div className="hirebutton">
+                        <div>Engager</div>
+                </div>
               </div>
             </div>
           </div>
